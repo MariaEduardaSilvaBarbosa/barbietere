f643347f22dfb1c46c7e8c2d1fcab9d45fb5ecb7
MariaEduardaSilvaBarbosa <maria.barbosa4@estudante.ifms.edu.br>
2023-08-31 08:56:37 -0400

    Meu primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Footer from './componentes/footer/Footer'
+import Header from './componentes/header/Header'
+
+function App() {
+  
+  return (
+    <>
+    <Header/>
+    <Footer/>
+    </>
+   
+  )
+}
+
+export default App
---
+++ src/componentes/footer/Footer.tsx
@@ -0,0 +1,11 @@
+import './Footer.css'
+import Logo from '../../assets/Logo_barbie.png.webp'
+function Footer(){
+    return(
+        <footer>
+            <img src={Logo} alt=""/>
+
+        </footer>
+    )
+}
+export default Footer
