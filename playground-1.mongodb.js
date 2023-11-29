38fab3ba965f05bd5e5c9f7619275a6047be89d4
MariaEduardaSilvaBarbosa <maria.barbosa4@estudante.ifms.edu.br>
2023-11-29 11:56:17 -0400

    trabalho frontend

--- playground-1.mongodb.js
+++ playground-1.mongodb.js
@@ -5,6 +5,31 @@
 use('db_barbie');
 
 // Create a new document in the collection.
-db.getCollection('filmes').insertOne({
-
-});
+db.getCollection('filmes').insertMany([
+  
+    {
+        _id: 9,
+        titulo: "Barbie",
+        descricao: "programdno",
+        imagem: "/barbie.jpg"
+    },
+    {
+        _id: 8,
+        titulo: "Bezouro Azul",
+        descricao: "Programando",
+        imagem: "/beazul.png"
+    },
+    {
+        _id: 6,
+        titulo: "Elementos",
+        descricao: "Programando",
+        imagem: "/elementos.jpg"
+    },
+    {
+        _id: 4,
+        titulo: "Oppenheimer",
+        descricao: "Programando",
+        imagem: "/oppenheimer.jpg"
+    }
+  
+]);
